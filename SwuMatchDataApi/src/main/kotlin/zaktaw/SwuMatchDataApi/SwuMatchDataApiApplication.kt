d29b9fba93231b0f327ab4410d1a8e07091319b9
Zakaria Karlsen Tawfiq <zaktaw@gmail.com>
2024-07-26 17:47:01 +0200

    Init backend project

---
+++ SwuMatchDataApi/src/main/kotlin/zaktaw/SwuMatchDataApi/SwuMatchDataApiApplication.kt
@@ -0,0 +1,11 @@
+package zaktaw.SwuMatchDataApi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SwuMatchDataApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SwuMatchDataApiApplication>(*args)
+}
---
+++ SwuMatchDataApi/src/test/kotlin/zaktaw/SwuMatchDataApi/SwuMatchDataApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package zaktaw.SwuMatchDataApi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SwuMatchDataApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
